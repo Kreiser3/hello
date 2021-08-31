751c78de793cdb3c7dd536bacf52e54f72e9e5ee
msaltzman5 <89426885+msaltzman5@users.noreply.github.com>
2021-08-30 21:37:27 -0500

    Update hello.c

--- Hello/hello.c
+++ Hello/hello.c
@@ -5,5 +5,9 @@ int main(int argc, char **argv) {
 
   printf("Kaleb Reiser!\n");
   printf("Electrical Engineering\n");
+  
+  printf("Malcolm Saltzman");
+  printf("Computer Science");
+  
   return 0;
 }
